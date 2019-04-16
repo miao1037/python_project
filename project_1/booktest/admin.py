a892dc597a5bdd5ae8bac0006fdcf162999de0af
miao <1037146872@qq.com>
2019-04-16 16:03:41 +0800

    创建站点和站点管理用户admin,向admin注册booktest模型

--- project_1/booktest/admin.py
+++ project_1/booktest/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import HeroInfo,BookInfo
 
 # Register your models here.
+
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
+
--- project_1/booktest/models.py
+++ project_1/booktest/models.py
@@ -5,12 +5,16 @@ from django.db import models
 class BookInfo(models.Model):
     Btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.Btitle
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
     hgender = models.BooleanField()
     hcontent = models.CharField(max_length=50)
     hbook = models.ForeignKey("BookInfo",on_delete=models.CASCADE)
+    def __str__(self):
+        return self.hname
 
 
 
