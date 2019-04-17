1bd92650cf814489618e80ce9e7dd135d74d81e3
miao <1037146872@qq.com>
2019-04-17 18:05:32 +0800

    去除超级链接硬编码 1.在应用urls.py添加命名空间 2.在项目urls.py指定命名空间 3.在应用里添加别名 4.去除硬编码{% url '命名：别名' %}

--- project_1/booktest/urls.py
+++ project_1/booktest/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'booktest'
+
 urlpatterns = [
-    url(r'^index/$',views.index),
-    url(r'^list/$',views.list),
-    url(r'^detail/(\d+)/$',views.detail)
+    url(r'^index/$',views.index,name='index'),
+    url(r'^list/$',views.list,name='list'),
+    url(r'^detail/(\d+)/$',views.detail,name='detail')
 ]
