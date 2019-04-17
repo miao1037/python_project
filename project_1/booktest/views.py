556a8a55d59ea593faeecda3ebdff3f73ae6d69e
miao <1037146872@qq.com>
2019-04-17 16:59:11 +0800

    创建html模板templates->booktest 定义index\list\detail模板 使用模板-->render

--- project_1/booktest/views.py
+++ project_1/booktest/views.py
@@ -1,21 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from  .models import BookInfo
-
+from .models import BookInfo
+from django.template import loader,RequestContext
 
 # Create your views here.
 
 
 def index(requset):
-    print("请求",requset)
-    return HttpResponse("首页")
+    # print("请求",requset)
+    # return HttpResponse("首页")
+
+    #加载模板
+    # indextem = loader.get_template('booktest/index.html')
+    # #构造上下文
+    # cont = {"username":"miao"}
+    # #渲染模板
+    # result = indextem.render(cont)
+    # return HttpResponse(result)
+
+    cont = {"username":"miao"}
+    return render(requset,'booktest/index.html',cont)
 
 def list(requset):
-    return HttpResponse("列表页")
+    # return HttpResponse("列表页")
+
+    # listtem = loader.get_template('booktest/list.html')
+    # booklist = BookInfo.objects.all()
+    # cont = {"booklist":booklist}
+    # result = listtem.render(cont)
+    # return HttpResponse(result)
+    booklist = BookInfo.objects.all()
+    cont = {"booklist": booklist}
+    return render(requset,'booktest/list.html',cont)
+
 
 def detail(requset,id):
-    try:
-        book = BookInfo.objects.get(pk = int(id))
-        return HttpResponse(book)
-    except:
-        print("请输入正确id")
+    # try:
+    #     book = BookInfo.objects.get(pk = int(id))
+    #     return HttpResponse(book)
+    # except:
+    #     return HttpResponse("请输入正确id")
+
+    # try:
+    #     detailtem = loader.get_template('booktest/detail.html')
+    #     book = BookInfo.object.get(pk = int(id))
+    #     cont = {"book":book}
+    #     result = detailtem.render(cont)
+    #     return HttpResponse(result)
+    # except:
+    #     return HttpResponse("请输入正确id")
+
+    book = BookInfo.objects.get(pk=id)
+    cont = {"book":book}
+    return render(requset,'booktest/detail.html',cont)
