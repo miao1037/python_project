04d493b4d94bdc2142f9ae605e82c8d3ab557ed0
miao <1037146872@qq.com>
2019-04-18 12:02:31 +0800

    新建静态文件 添加删除，添加功能

--- project_1/booktest/views.py
+++ project_1/booktest/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import BookInfo
-from django.template import loader,RequestContext
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import BookInfo,HeroInfo
+from django.template import loader
 
 # Create your views here.
 
@@ -53,3 +53,33 @@ def detail(requset,id):
     book = BookInfo.objects.get(pk=id)
     cont = {"book":book}
     return render(requset,'booktest/detail.html',cont)
+
+def delete(requset,id):
+    try:
+        BookInfo.objects.get(pk=id).delete()
+        booklist = BookInfo.objects.all()
+        cont = {"booklist": booklist}
+        # return render(requset, 'booktest/list.html', cont)
+        #重定向 重新向服务器发起请求 刷新url
+        return HttpResponseRedirect('/booktest/list/',cont)
+    except:
+        return HttpResponse('删除失败')
+
+def addhero(requset,bookid):
+    return render(requset,'booktest/addhero.html',{"bookid":bookid})
+
+def addherohandler(request):
+    bookid = request.POST["bookid"]
+    hname = request.POST["heroname"]
+    hgender = request.POST['sex']
+    hcontent = request.POST['herocontent']
+
+    book = BookInfo.objects.get(pk=bookid)
+    hero = HeroInfo()
+    hero.hname = hname
+    hero.hgender = True
+    hero.hcontent = hcontent
+    hero.hbook = book
+    hero.save()
+    return HttpResponseRedirect('/booktest/detail/'+str(bookid)+'/',{"book":book})
+
